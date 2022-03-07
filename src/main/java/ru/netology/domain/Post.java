baea430fd2cf9fc73bf8f18e59dbb4033604bf8d
kukh29 <kukh29@gmail.com>
2022-03-07 14:14:53 +0300

    OOP

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+import ru.netology.manager.FormDate;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public Boolean subscription;
+    public FormDate birthday;
+
+}
